f6c94868c43616855476d649ab94a836fbab0f3d
Nick Knight <nick@localhost.localdomain>
2019-08-21 19:08:40 +0100

    Values where wrong

--- rtp/globals.h
+++ rtp/globals.h
@@ -7,8 +7,8 @@
 /* The number of bytes in a packet ( these figure are less some overhead G711 = 172*/
 #define G711PAYLOADBYTES 160
 #define G722PAYLOADBYTES 160
-#define L16NARROWBANDBYTES 160
-#define L16WIDEBANDBYTES 320
+#define L16NARROWBANDBYTES 320
+#define L16WIDEBANDBYTES 640
 #define ILBC20PAYLOADBYTES 38
 #define ILBC30PAYLOADBYTES 50
 
@@ -16,10 +16,10 @@
 #define PCMAPAYLOADTYPE 8
 #define G722PAYLOADTYPE 9
 #define ILBCPAYLOADTYPE 97
-/* RFC says this is 44k sampling, however it also seems a little ambiguous on this - so we generally are going to be using this so lets see where it goes! */
+/* RFC says this is 44k sampling, however it also seems a little ambiguous on this - so we generally are going to be using this internally so lets see where it goes! So warning - only use for internal work*/
 #define L16PAYLOADTYPE 11
 
-/* Need to double check max RTP length with variable length header - there could be a larger length withour CODECs */
+/* Need to double check max RTP length with variable length header - there could be a larger length with our CODECs */
 /* this maybe breached if a stupid number of csrc count is high */
 #define RTPMAXLENGTH 200
 #define L16MAXLENGTH ( RTPMAXLENGTH * 2 )
